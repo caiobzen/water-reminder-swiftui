3a167d50e6301150c48abfdb4cf72729aa8d702a
Carlos Corrêa <caiobzen@gmail.com>
2019-09-19 20:24:54 -0300

    feat: make water fill up the screen

--- Waterminder/Waterminder WatchKit Extension/WaterView.swift
+++ Waterminder/Waterminder WatchKit Extension/WaterView.swift
@@ -2,34 +2,50 @@ import SwiftUI
 
 struct WaterView: View {
     @State var amount = 100.0
+    @State var target = 2000.0
+    @State var fillValue: CGFloat = 0.0
     
     private var amountText: String {
         "\(Int(amount).description)ml"
     }
     
+    private var targetText: String {
+        target == .zero
+            ? "Nice job!"
+            : "Target: \(Int(target).description)ml"
+    }
+    
+    private func drink() {
+        guard target - amount >= .zero else { return }
+        target -= amount
+        fillValue += CGFloat(amount / 10)
+    }
+    
     var body: some View {
         ZStack(alignment: .center) {
-            WavingBackground()
+            WavingBackground(fill: fillValue)
             VStack {
-                Text("Target: 2000ml")
-                .font(.system(size: 14, weight: .semibold, design: .rounded))
-                                
-                Button(action: {
-                   print("drink!")
-                }) {
-                    HStack(alignment: .center, spacing: 24) {
-                        Image(systemName: "plus.circle.fill")
-                        .resizable()
-                        .frame(width: 20, height: 20)
-                        Text(amountText)
-                    }
+                Text(targetText)
+                .font(.system(size: 20, weight: .semibold, design: .rounded))
+                    
+                if target != .zero {
+                    Button(action: {
+                        withAnimation { self.drink() }
+                    }) {
+                        HStack(alignment: .center, spacing: 24) {
+                            Image(systemName: "plus.circle.fill")
+                            .resizable()
+                            .frame(width: 20, height: 20)
+                            Text(amountText)
+                        }
+                        }
+                    .frame(width: 130, height: 50)
                 }
-                .frame(width: 130, height: 50)
             }
+            .shadow(radius: 1)
         }
         .focusable()
-        .digitalCrownRotation($amount, from: 100, through: 1000, by: 50, sensitivity: .medium)
-        .background(Color.black)
+        .digitalCrownRotation($amount, from: 50, through: 1000, by: 50, sensitivity: .medium)
         .edgesIgnoringSafeArea(.all)
     }
 }
--- Waterminder/Waterminder WatchKit Extension/Widgets/WavingBackground.swift
+++ Waterminder/Waterminder WatchKit Extension/Widgets/WavingBackground.swift
@@ -1,37 +1,44 @@
 import SwiftUI
 
 struct WavingBackground: View {
+    var fill: CGFloat = 0.0
     @State var offsetY: CGFloat = 70
     @State var backgroundOffsetY: CGFloat = 75
     
     var repeatingAnimation: Animation {
         Animation
             .easeInOut
-            .delay(0)
             .speed(0.15)
             .repeatForever()
     }
     
     var body: some View {
-        ZStack {
-            Wave(graphWidth: 1, amplitude: 0.05)
-                .offset(x: 0, y: offsetY)
-                .onAppear { self.offsetY = 65 }
+        VStack {
+            ZStack {
+                Wave(graphWidth: 1, amplitude: 0.05)
+                    .offset(x: 0, y: offsetY)
+                    .onAppear { self.offsetY = 65 }
+                
+                Wave(graphWidth: 1, amplitude: 0.05)
+                     .opacity(0.7)
+                     .offset(x: 70, y: backgroundOffsetY)
+                     .onAppear { self.backgroundOffsetY = 65 }
+            }
+            .animation(repeatingAnimation)
+            .frame(width: 200, height: 200)
             
-            Wave(graphWidth: 1, amplitude: 0.05)
-                 .opacity(0.7)
-                 .offset(x: 70, y: backgroundOffsetY)
-                 .onAppear { self.backgroundOffsetY = 65 }
+            Rectangle()
+            .frame(width: 200, height: fill)
         }
-        .animation(repeatingAnimation)
+        .offset(x: 0, y: 40)
         .foregroundColor(Color(red: 0, green: 0.8, blue: 1))
-        .background(Color.black)
+        .frame(minWidth: .zero, maxWidth: .infinity, minHeight: .zero, maxHeight: .infinity)
         .edgesIgnoringSafeArea(.all)
     }
 }
 
 struct WavingBackground_Previews: PreviewProvider {
     static var previews: some View {
-        WavingBackground()
+        WavingBackground(fill: 0)
     }
 }
