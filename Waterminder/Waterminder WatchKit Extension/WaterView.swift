0a867670a1014fdb8523d7fdd565ec9bdea968e3
Carlos Corrêa <caiobzen@gmail.com>
2019-09-19 19:26:21 -0300

    feat: bind amount water to digital crown

--- Waterminder/Waterminder WatchKit Extension/WaterView.swift
+++ Waterminder/Waterminder WatchKit Extension/WaterView.swift
@@ -1,22 +1,34 @@
 import SwiftUI
 
 struct WaterView: View {
+    @State var amount = 100.0
+    
+    private var amountText: String {
+        "\(Int(amount).description)ml"
+    }
+    
     var body: some View {
         ZStack(alignment: .center) {
             WavingBackground()
             VStack {
                 Text("Target: 2000ml")
                 .font(.system(size: 14, weight: .semibold, design: .rounded))
-                
+                                
                 Button(action: {
-                    print("Há!")
+                   print("drink!")
                 }) {
-                    Image(systemName: "plus.circle.fill")
-                    .resizable()
-                    .frame(width: 40, height: 40)
-                }.frame(width: 40, height: 40)
+                    HStack(alignment: .center, spacing: 24) {
+                        Image(systemName: "plus.circle.fill")
+                        .resizable()
+                        .frame(width: 20, height: 20)
+                        Text(amountText)
+                    }
+                }
+                .frame(width: 130, height: 50)
             }
         }
+        .focusable()
+        .digitalCrownRotation($amount, from: 100, through: 1000, by: 50, sensitivity: .medium)
         .background(Color.black)
         .edgesIgnoringSafeArea(.all)
     }
